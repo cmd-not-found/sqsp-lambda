4524052b05bf20c5826d8843e464bf9bee5311a6
cmd-not-found <cmd404notfound@gmail.com>
2022-03-05 13:57:50 -0500

    initial sync with github actions and lambda

---
+++ SQSPApiGatewayWebhookListener.py
@@ -0,0 +1,35 @@
+import os
+import json
+import boto3
+import datetime
+
+session = boto3.Session()
+s3 = session.resource('s3')
+
+def handler(event, context):
+    '''
+    Parse a proxy API Gateway object's `body` parameter and save to S3.
+    '''
+
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+
+    if 'body' in event:
+        event_data = event.get('body')
+        data = json.loads(event_data)
+        if 'data' in data:
+            obj = s3.Object(os.environ('BUCKET'), f'order_{timestamp}.json')
+            res = obj.put(Body=json.dumps(data, indent=4))
+            return {
+                'statusCode': 200,
+                'body': json.dumps({'status': 'success'})
+            }
+        else:
+            return {
+                'statusCode': 500,
+                'body': json.dumps({'status': 'failed'})
+            }
+    else:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'status': 'failed'})
+        }
